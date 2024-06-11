1862a173ee6c3ece3fefeb3799724ce6e5d10087
C0A23148 <c0a23148ce@edu.teu.ac.jp>
2024-06-11 16:19:31 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -20,14 +20,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
 
         chara_rct = chara_img.get_rect() #こうかとんレクトの取得
         chara_rct.center = 300, 200
         screen.blit(chara_img, chara_rct)
 
         pg.display.update()
-        tmr += 1        
+        tmr += 1   
+
+
         clock.tick(200)
 
 
